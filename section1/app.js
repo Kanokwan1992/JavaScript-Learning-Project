c63af4c92181317470bd5a0df6f9cf6a6f386d3c
Kanokwan Jenparu <kanokwan.jenparu@gmail.com>
2024-02-27 15:50:30 +0700

    complete section1

---
+++ section1/app.js
@@ -0,0 +1,7 @@
+document.write("<h1><i>Lolibansai.com</i></h1>")
+document.write("<p>Javascript เบื้องต้น</p>")
+
+alert("คุณไนซ์กลับบ้านได้แล้ว")
+console.log("Hello จารย์ไนซ์")
+console.error("เกิดข้อผิดพลาดจารย์ไนซ์จับได้แล้ว")
+console.warn("ระวัง...จารย์ไนซ์ถือก้านมะยมรอแล้ว")
