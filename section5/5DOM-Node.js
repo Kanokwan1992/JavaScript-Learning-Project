4fcec615eb5a5cebc31a0f6cf5f0a8ec3005af97
Kanokwan Jenparu <kanokwan.jenparu@gmail.com>
2024-04-25 08:09:03 +0700

    Update 5DOM-Node.js

--- section5/5DOM-Node.js
+++ section5/5DOM-Node.js
@@ -4,5 +4,11 @@ let count = 1;
 function additem() {
   const item = document.createElement('li');
   item.innerText = 'item' + count++;
+  item.id = `item${count}`;
   menu.appendChild(item);
+}
+function deleteitem() {
+  const item = document.getElementById(`item${count}`);
+  menu.removeChild(item);
+  count--;
 }
